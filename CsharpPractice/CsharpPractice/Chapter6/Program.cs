0c7dc903b02e63c2e079fd5281918ac90a5f52cb
Ravibhushan Kumar <ravibhushankumarsonu@gmail.com>
2016-03-21 23:27:03 +0530

    CSharp

---
+++ CsharpPractice/CsharpPractice/Chapter6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPractice
+{
+    class Building
+    {
+        public int Floor;
+        public int Area;
+        public int Occupants;
+        public double AreaPerPerson()
+        {
+            return (double)Area / Occupants;
+        }
+        //public Building(int x)
+        //{
+        //    Floor = x;
+        //}
+        ~Building()
+        {
+            Console.WriteLine("Destructing");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building house = new Building();
+            double areaPP;
+            house.Occupants = 4;
+            house.Area = 2500;
+            house.Floor = 2;
+            areaPP =house.AreaPerPerson();
+
+            Console.WriteLine("House has \n"+house.Floor+" Floors\n"+house.Area+" Area\n"+house.Occupants+" Occupants\n"+areaPP+" Area Per Person Available");
+        }
+    }
+}
---
+++ CsharpPractice/CsharpPractice/Chapter7/OneDArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPractice.Chapter7
+{
+    class OneDArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = i + 1;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Array ["+i+"] contains "+arr[i]+"\n");
+            }
+        }
+    }
+}
