bd34fc6c271f1751568ee4c3021abbfdcaddc467
Ravibhushan Kumar <ravibhushankumarsonu@gmail.com>
2016-03-22 01:06:38 +0530

    no

---
+++ CsharpPractice/CsharpPractice/Chapter8/UseOut.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPractice.Chapter8
+{
+    class Decompose
+    {
+        /*Decompose floating point value into its integer and 
+        fractional part. 
+        */
+        public int Getparts(double n,out double frac)
+        {
+            int whole;
+            whole = (int)n;
+            frac = n - whole;
+            return whole;
+        }
+    }
+    class UseOut
+    {
+        static void Main(string[] args)
+        {
+            Decompose ob = new Decompose();
+            int i;
+            double f;
+            i = ob.Getparts(10.50,out f);
+            Console.WriteLine("Integer portion is :"+i);
+            Console.WriteLine("Fractional portion is :"+f);
+        }
+    }
+}
